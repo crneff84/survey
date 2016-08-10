0affcb99e6507204b1f32a8c230eb2d86b94cebe
Chance Neff and David Ayala <chance@>
2016-08-10 13:52:34 -0700

    adds DOB element

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var personInput = $("input#person").val();
+    var dob = $("#born").val();
     var food = $("#food").val();
     var music = $("input:radio[name=genre]:checked").val();
     var favoriteColor = $("input#color").val();
 
     $(".person").text(personInput);
+    $(".born").text(dob);
     $(".food").text(food);
     $(".genre").text(music);
     $(".color").text(favoriteColor);
